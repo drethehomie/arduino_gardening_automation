6ff5cfa308db98aa9a0721a5bbaa92ec7baeaae1
Andre Cabrales <ohndray@gmail.com>
2022-02-11 18:22:07 -0700

    added folder.

--- test_code/led_2_second_blink/led_1_second_blink/led_1_second_blink.ino
+++ test_code/led_2_second_blink/led_1_second_blink/led_1_second_blink.ino
@@ -9,13 +9,6 @@ Blink
 */
 
 
-pinMode(LED_BUILTIN, OUTPUT);
-
-digitalWrite(LED_BUILTIN, HIGH);
-
-digitalWrite(LED_BUILTIN, LOW);
-
-
 void setup() {
   // put your setup code here, to run once:
 
